641cce7ae010ca19bcc3bbfc7bcf9613df769b2e
lipeiyu <60626754+Luckilyeee@users.noreply.github.com>
2023-10-18 12:22:57 -0600

    Update constants.py

--- syn:real ratios/utils/constants.py
+++ syn:real ratios/utils/constants.py
@@ -5,7 +5,13 @@ DATASET_NAMES = ['solarflare']
 
 sizes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-CLASSIFIERS = ['fcn', 'resnet', 'fcn']
-ARCHIVE_NAMES = ['SF_aug_ori', 'SF_aug_tw', 'SF_aug_ws']
+CLASSIFIERS = ['fcn']
+ARCHIVE_NAMES = ['SF_aug_ws']
 DATA_FOLDERS = {'time_warp', 'window_slice'}
 data_folders_for_archive = {'SF_aug_ws': 'window_slice', 'SF_aug_tw': 'time_warp'}
+
+
+# CLASSIFIERS = ['resnet', 'lstm_fcn']
+# ARCHIVE_NAMES = ['SF_aug_tw']
+# DATA_FOLDERS = {'time_warp', 'window_slice'}
+# data_folders_for_archive = {'SF_aug_ws': 'window_slice', 'SF_aug_tw': 'time_warp'}
